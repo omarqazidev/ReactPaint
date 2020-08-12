db0f8fcd401dd9a16b9c40ea41c36046d0e13384
Omar Qazi <omarqazidev@gmail.com>
2020-08-12 08:37:38 +0500

    renamed header.css

--- src/components/user-interface/top/Header.tsx
+++ src/components/user-interface/top/Header.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 
 import React from 'react';
-import '../../../css/header.css';
+import '../../../css/headercss.css';
 import { useComponent } from '../../../redux';
 import { generateCode } from '../../../generate/ReactCodeGenerator';
 import { savefile } from '../../../utils/savefile';
